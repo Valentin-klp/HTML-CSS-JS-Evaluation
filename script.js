2e21da5adc85e0df69ebd7f0e11158328bf68904
Valentin-klp <kompldjfzep@gmail.com>
2025-02-05 16:50:08 +0100

    final

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function afficherAlerte() {
-    alert("Le Daron à Clément !");
+function afficherAlerteEtRediriger() {
+    alert("Prépare-toi à voir Enzo en grand !");
+    window.location.href = "page1.html"; // Redirection après l'alerte
 }
